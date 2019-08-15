f8449ccbe0a0f233df8583857dfa127d5fcfb69c
lyob <thisisbenlyo@gmail.com>
2019-08-15 16:33:22 -0500

    Update fim3.py

--- FIM/fim3.py
+++ FIM/fim3.py
@@ -17,16 +17,16 @@ for label in range(10):
 # scores = {"label0":{"l0p0":X, "l0p1":X, ...}, "label1": {"l1p0":X, "l1p1":X, ...}, ... }
 # where X is a value
 
-print(scores["label0"]["l0p20001"])
+# print(scores["label0"]["l0p20001"])
 
 #%%
 # build the matrix by calculating score(i, label) * score(j, label) for all pairs of parameters i,j
 def calc_score_pairs(scores, label):
     print('------------------calculating score pairs for label = {}-------------------'.format(l))
     score_pairs = {}
-    for i in range(10001):
+    for i in range(20002):
         if i%100==0: print(i)
-        for j in range(10001):
+        for j in range(20002):
             if 'i{}j{}'.format(j, i) in score_pairs:
                 score_pairs['i{}j{}'.format(i,j)] = score_pairs['i{}j{}'.format(j,i)]
             else:
@@ -36,8 +36,5 @@ def calc_score_pairs(scores, label):
 # this is fine
 
 score_pairs = calc_score_pairs(scores, l)
-print(score_pairs)
-#%%
-pickle.dump( score_pairs, open("./pickle/score_pairs{}.p".format(l), "wb") )
-print( "-------- Score pairs have been pickled as score_pairs{}.p --------".format(l) )
+
 
