41f51f46c8d1c8551ec8826a9a298119cd3d52be
lyob <thisisbenlyo@gmail.com>
2019-08-15 16:33:05 -0500

    added matrix solver

--- FIM/fim6.py
+++ FIM/fim6.py
@@ -1,29 +1,47 @@
+#%%
 ## libraries
 import numpy as np
 import scipy
-import pickle
-
+from memory_profiler import profile
+#%%
 # paths
 savePath = './output/'
 
-# loading pickle files
-mfim = pickle.load( open('fim5.p', 'rb') )
+# loading matrix array
+def load_matrix():
+    fim = np.load( savePath + 'fim.npy' )
+    return fim
+
+
+# calculating the eigenvalues and eigenvectors of the symmetric square matrix
+@profile
+def diagonalise(fim):
+    from scipy.linalg import eigh
+    print('------ calculating the eigenvectors and eigenvalues of the FIM ------')
+    w, v = eigh(fim)
+    return w, v
+# results = {"eigenvalues": w, 'eigenvectors': v}
+
+def export_npy(output_name, array):
+    np.save(savePath + output_name, array)
+    print("The array has been exported as {}".format(output_name))
+
+def export_csv(output_name, array):
+    np.savetxt(savePath + output_name, array, delimiter=',')
+    print("The array has been exported as {}".format(output_name))
 
 #%%
-# calculating the eigenvalues and eigenvectors of the matrix
-from scipy.linalg import eigh
-print('----------------calculating the eigenvectors and eigenvalues of the FIM---------------')
-w, v = eigh(mfim)
+if __name__ == "__main__":
+    fim = load_matrix()
+    w, v = diagonalise(fim)
+    print( '------ The matrix has been successfully diagonalised ------')
 
-results = {"eigenvalues": w, 'eigenvectors': v}
+    export_npy('eigenvalues.npy', w)
+    export_npy('eigenvectors.npy', v)
+
+    export_csv('eigenvalues.csv', w)
+    export_csv('eigenvectors.csv', v)
+
+    print( '------ The eigenvectors and eigenvalues of the FIM matrix have been saved ------' )
 
 #%%
-# writes output to a file
-w_file = savePath+'eigenvalues.csv'
-v_file = savePath+'eigenvectors.csv'
-
-np.savetxt(w_file, w, delimiter=',')
-np.savetxt(v_file, v, delimiter=',')
-
-pickle.dump( results, open('fim6.p', 'wb') )
-print( '------ The eigenvectors and eigenvalues of the FIM matrix have been pickled as fim6.p ------' )
