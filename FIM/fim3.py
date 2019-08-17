af2e5d87284dc6dfd12a038cfbbe3c7cd49375be
lyob <thisisbenlyo@gmail.com>
2019-08-17 00:54:06 -0500

    New fim numbering

--- FIM/fim3.py
+++ FIM/fim3.py
@@ -1,40 +1,69 @@
 #%%
 ## libraries
-import pickle
 import numpy as np
-
-# parameter
-l = 0
+import pickle
+import multiprocessing as mp
+import psutil
 
 #%%
-# import all 10 pickle files (which are dictionaries of scores)
-scores = {}
-for label in range(10):
+# import all pickle files
+def import_common_files():
+    original = pickle.load( open('./pickle/fim1.p', 'rb') )
+    softmax = original['softmax']
+    return softmax
+
+def import_per_label_files(label):
     ls = pickle.load( open("./pickle/scores{}.p".format(label), "rb") )
-    scores.update( {"label{}".format(label) : ls} )
+    # a dictionary of scores that looks like:
+    # scores = {"label0":{"l0p0":X, "l0p1":X, ...}, "label1": {"l1p0":X, "l1p1":X, ...}, ... }
+    # where X is a value
+    return ls
 
-# now we have a dictionary of scores that looks like:
-# scores = {"label0":{"l0p0":X, "l0p1":X, ...}, "label1": {"l1p0":X, "l1p1":X, ...}, ... }
-# where X is a value
-
-# print(scores["label0"]["l0p20001"])
-
-#%%
 # build the matrix by calculating score(i, label) * score(j, label) for all pairs of parameters i,j
-def calc_score_pairs(scores, label):
-    print('------------------calculating score pairs for label = {}-------------------'.format(l))
-    score_pairs = {}
-    for i in range(20002):
-        if i%100==0: print(i)
-        for j in range(20002):
-            if 'i{}j{}'.format(j, i) in score_pairs:
-                score_pairs['i{}j{}'.format(i,j)] = score_pairs['i{}j{}'.format(j,i)]
+# using structured numpy arrays instead of dictionaries to reduce memory usage
+def calc_score_pairs(softmax, label):
+    size = 100
+    scores = import_per_label_files(label)
+    print('----- calculating score pairs for label = {} -----'.format(label))
+    score_pairs = np.zeros((size, size))
+    tracker = np.zeros((size, size))
+
+    for i in range(size):
+        for j in range(size):
+            if tracker[j][i] == 1:
+                score_pairs[i][j] = score_pairs[j][i]
             else:
-                score_pairs.update( {'i{}j{}'.format(i,j) : scores["label{}".format(label)]['l{}p{}'.format(label, i)] * scores["label{}".format(label)]['l{}p{}'.format(label, j)]} )
+                score_pairs[i][j] = scores['l{}p{}'.format(label, i)] * scores['l{}p{}'.format(label, j)] * softmax[0][label]
+                tracker[i][j] = 1
+    print('The size of the array for label={} is {} bytes'.format(label, score_pairs.nbytes))
     return score_pairs
-# we have the if cond because sp(i,j|l) = sp(j,i|l) -- this would remove 10*20002 unnecessary calculations, but it 
-# this is fine
+# we have the if cond because sp(i,j|l) = sp(j,i|l) -- this would remove 10*20002 unnecessary calculations
 
-score_pairs = calc_score_pairs(scores, l)
+# def multi():
+#     for i in range(10):
+#         p = mp.Process(target=calc_score_pairs, args=(scores, i))
+#         p.start()
+#         p.join()
 
+#     print("done!")
+
+def export_fim(matrix):
+    np.save('./output/fim.npy', matrix)
+    print("The FIM matrix has been exported as fim.npy")
+
+if __name__ == '__main__':
+    num_cores = mp.cpu_count()
+    print('This kernel has {} cores.'.format(num_cores))
+    print(psutil.virtual_memory())
+
+    softmax = import_common_files()
+    
+    fim = calc_score_pairs(softmax, 0)
+    # I want to parallelise this process and also keep memory use low by using subroutines
+    for i in range(1,10):
+        mat = calc_score_pairs(softmax, i)
+        fim = np.add(fim, mat)
+        print(psutil.virtual_memory())
+    export_fim(fim)
+    print("fim[0][0] =", fim[0][0])
 
--- FIM/fim4.py
+++ FIM/fim4.py
@@ -1,29 +1,47 @@
 #%%
 ## libraries
-import pickle
+import numpy as np
+import scipy
+from memory_profiler import profile
+#%%
+# paths
+savePath = './output/'
 
-
-# import pickle files
-score_pairs = pickle.load( open("fim3.p", "rb") )
-original = pickle.load( open('fim1.p', 'rb') )
-softmax = original['softmax']
-
-def calc_fim(score_pairs):
-    print('-----------------------calculating the FIM---------------------------')
-    fim = {} # this will be a 20002 * 20002 matrix
-    for i in range(20002):
-        for j in range(20002):
-            if 'i{}j{}'.format(j,i) in fim:
-                fim['i{}j{}'.format(i,j)] = fim['i{}j{}'.format(j,i)]
-            else:
-                sum = 0
-                for label in range(10):
-                    sum += softmax[0][label] * score_pairs[label]['i{}j{}'.format(i,j)]
-                fim['i{}j{}'.format(i,j)] = sum
+# loading matrix array
+def load_matrix():
+    fim = np.load( savePath + 'fim.npy' )
     return fim
-# we can use the same trick we used in score_pairs above: if fim(ji|l) already exists then copy that value to fim(ij|l) 
 
-fim = calc_fim(score_pairs)
 
-pickle.dump( fim, open("fim4.p", "wb") )
-print('----- fim matrix (dictionary) has been pickled as fim4.p -----')
+# calculating the eigenvalues and eigenvectors of the symmetric square matrix
+@profile
+def diagonalise(fim):
+    from scipy.linalg import eigh
+    print('------ calculating the eigenvectors and eigenvalues of the FIM ------')
+    w, v = eigh(fim)
+    return w, v
+# results = {"eigenvalues": w, 'eigenvectors': v}
+
+def export_npy(output_name, array):
+    np.save(savePath + output_name, array)
+    print("The array has been exported as {}".format(output_name))
+
+def export_csv(output_name, array):
+    np.savetxt(savePath + output_name, array, delimiter=',')
+    print("The array has been exported as {}".format(output_name))
+
+#%%
+if __name__ == "__main__":
+    fim = load_matrix()
+    w, v = diagonalise(fim)
+    print( '------ The matrix has been successfully diagonalised ------')
+
+    export_npy('eigenvalues.npy', w)
+    export_npy('eigenvectors.npy', v)
+
+    export_csv('eigenvalues.csv', w)
+    export_csv('eigenvectors.csv', v)
+
+    print( '------ The eigenvectors and eigenvalues of the FIM matrix have been saved ------' )
+
+#%%
